9b3415fbff3f427e8a4cd0031eae43096d7f5311
Alan Cahill <acahill98@yahoo.co.uk>
2014-10-12 11:05:50 +0100

    Project 1

---
+++ plot2.R
@@ -0,0 +1,18 @@
+#Read in data and put date and time in the required formats
+
+power<-read.table("household_power_consumption.txt", header=TRUE, sep=";", colClasses="character")
+power$combined<-as.factor(paste(power$Date, power$Time, sep=" "))
+
+power$times<-strptime(power$combined, "%d/%m/%Y %H:%M:%S")
+
+power$thedate<-as.Date(power$times, "%d/%m/%Y")
+
+dates<-subset(power, power$thedate %in% as.Date(c("2007-02-01", "2007-02-02")))
+
+#Open png device and create file in working directory
+png(file="plot2.png", width = 480, height = 480)
+#Graph 2
+with(dates, plot(times, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", main="Global Active Power"))
+
+#Close the png device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,21 @@
+#Read in data and put date and time in the required formats
+
+power<-read.table("household_power_consumption.txt", header=TRUE, sep=";", colClasses="character")
+power$combined<-as.factor(paste(power$Date, power$Time, sep=" "))
+
+power$times<-strptime(power$combined, "%d/%m/%Y %H:%M:%S")
+
+power$thedate<-as.Date(power$times, "%d/%m/%Y")
+
+dates<-subset(power, power$thedate %in% as.Date(c("2007-02-01", "2007-02-02")))
+
+#Open png device and create file in working directory
+png(file="plot3.png", width = 480, height = 480)
+#Graph 3
+with(dates, plot(times, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", main="Plot 3", col="black"))
+lines(dates$times, dates$Sub_metering_2, col="red")
+lines(dates$times, dates$Sub_metering_3, col="blue")
+legend("topright", lwd=1,col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#Close the png device
+dev.off()
